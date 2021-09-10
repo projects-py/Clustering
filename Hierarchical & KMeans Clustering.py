cf84654b5d36c7240a79c9160a802d507253dd7f
projects-py <73610847+projects-py@users.noreply.github.com>
2021-09-10 15:35:25 -0400

    Update Hierarchical & KMeans Clustering.py

--- Hierarchical & KMeans Clustering.py
+++ Hierarchical & KMeans Clustering.py
@@ -1,31 +1,20 @@
-#!/usr/bin/env python
-# coding: utf-8
 
 # # Clustering Algorithm
 # #Hierarchical Clustering
 
-# In[2]:
-
-
+#importing libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[3]:
-
 
 df = pd.read_csv("Documents/customers data.csv")
 
 
-# In[4]:
+print(df.head())
 
-
-df.head()
-
-
-# In[5]:
 
 
 #We need to make clusters, but first we Normalize the data
@@ -35,8 +24,6 @@ from sklearn.preprocessing import normalize
 data_scaled = pd.DataFrame(normalize(df), columns = df.columns)
 data_scaled.head()
 
-
-# In[6]:
 
 
 #Forming clusters
@@ -48,8 +35,6 @@ plt.title("Dendrograms")
 dend = shc.dendrogram(shc.linkage(data_scaled, method = 'ward'))
 
 
-# In[8]:
-
 
 #as verticle line with the maximum distance is the blue line and hence we can decide a threshold at 6 and cut the dendrogram
 
@@ -59,28 +44,17 @@ dend = shc.dendrogram(shc.linkage(data_scaled, method = 'ward'))
 plt.axhline(y=6, color = 'r', linestyle = '--')
 
 
-# In[9]:
-
-
 from sklearn.cluster import AgglomerativeClustering
 cluster = AgglomerativeClustering(n_clusters=2, affinity = 'euclidean', linkage = 'ward')
 cluster.fit_predict(data_scaled)
-
-
-# In[30]:
 
 
 plt.figure(figsize = (10,8))
 plt.scatter(data_scaled['Grocery'], data_scaled['Milk'], c= cluster.labels_)
 
 
-# In[12]:
-
-
 #So we have clearly 2 clusters
 
-
-# In[16]:
 
 
 # Using K-means clustering
@@ -89,10 +63,6 @@ from sklearn import metrics
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
-df
-
-
-# In[22]:
 
 
 #finding the number of clusters we want using elbow method
@@ -111,9 +81,6 @@ plt.title('Elbow Method')
 plt.show()
 
 
-# In[23]:
-
-
 #From here we see that 2 clusters must be made
 
 data_kmeans = KMeans(n_clusters=2)
@@ -122,13 +89,7 @@ labels = data_kmeans.predict(data_scaled)
 print(labels)
 
 
-# In[24]:
-
-
 centroids = data_kmeans.cluster_centers_
-
-
-# In[33]:
 
 
 s = metrics.silhouette_score(data_scaled, labels, metric = 'euclidean')
@@ -137,10 +98,6 @@ plt.figure(figsize = (10,8))
 
 plt.scatter(data_scaled['Grocery'],data_scaled['Milk'], c = labels, cmap = 'rainbow')
 plt.show()
-
-
-# In[ ]:
-
 
 
 
